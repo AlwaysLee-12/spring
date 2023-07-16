291511f5ff2682b0907b9ba8055ab616b0fb4748
alwayslee <ddoddo1997@naver.com>
2023-07-16 17:16:19 +0900

    중복있는 User DAO 버전

---
+++ src/main/java/com/practice/spring/Application.java
@@ -0,0 +1,34 @@
+package com.practice.spring;
+
+import com.practice.spring.user.dao.UserDao;
+import com.practice.spring.user.domain.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		SpringApplication.run(Application.class, args);
+
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("alwaysLee");
+		user.setName("lee");
+		user.setPassword("password");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + "등록 성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+
+		System.out.println(user2.getId() + "조회 성공");
+	}
+
+}
---
+++ src/main/java/com/practice/spring/user/domain/User.java
@@ -0,0 +1,12 @@
+package com.practice.spring.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String name;
+    String password;
+}
