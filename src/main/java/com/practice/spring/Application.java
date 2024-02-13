17a9968e9d9e1e5b3dfd124a16e8275877004016
alwayslee <ddoddo1997@naver.com>
2024-02-13 23:31:26 +0900

    main() > userDao test code 개선

--- src/main/java/com/practice/spring/Application.java
+++ src/main/java/com/practice/spring/Application.java
@@ -17,33 +17,45 @@ import java.sql.SQLException;
 public class Application {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//		SpringApplication.run(Application.class, args);
-//
-//		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
-//		UserDao dao = applicationContext.getBean("userDao", UserDao.class);
-//
-//		User user = new User();
-//		user.setId("alwaysLee");
-//		user.setName("lee");
-//		user.setPassword("password");
-//
-//		dao.add(user);
-//
-//		System.out.println(user.getId() + "등록 성공");
-//
-//		User user2 = dao.get(user.getId());
+		SpringApplication.run(Application.class, args);
+
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = applicationContext.getBean("userDao", UserDao.class);
+
+		User user = new User();
+		user.setId("alwaysLee");
+		user.setName("lee");
+		user.setPassword("password");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + "등록 성공");
+
+		User user2 = dao.get(user.getId());
+
+		//main 메서드 userDao test code 변경 전
 //		System.out.println(user2.getName());
 //		System.out.println(user2.getPassword());
 //
 //		System.out.println(user2.getId() + "조회 성공");
-		ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+		//main 메서드 userDao test code 변경 후 -> add()와 get()에 대한 테스트 전체적으로 가능. 단, 많은 양의 테스트 main() 만으로는 어려움
+		if (user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		} else if (user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		} else {
+			System.out.println("조회 테스트 성공");
+		}
+
+//		ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
 
 //		GenericXmlApplicationContext ac = new GenericXmlApplicationContext("com/practice/spring/common/config/application-context.xml"); xml을 이용하는 ac 생성
 
-		var userDao = ac.getBean("userDao", UserDao.class);
-
-		var connectionMaker = ac.getBean("connectionMaker", CountingConnectionMaker.class);
-		System.out.println("Connection count " + connectionMaker.getConnectionCount());
+//		var userDao = ac.getBean("userDao", UserDao.class);
+//
+//		var connectionMaker = ac.getBean("connectionMaker", CountingConnectionMaker.class);
+//		System.out.println("Connection count " + connectionMaker.getConnectionCount());
 	}
 
 }
