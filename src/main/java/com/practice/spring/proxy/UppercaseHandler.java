cd499cc6b702197f9966765eff12d449d09f121f
alwayslee <ddoddo1997@naver.com>
2024-03-23 15:37:36 +0900

    부가 기능 적용 타겟 확장(타겟 타입 확장, uppercase 안되는 경우 처리)

--- src/main/java/com/practice/spring/proxy/UppercaseHandler.java
+++ src/main/java/com/practice/spring/proxy/UppercaseHandler.java
@@ -5,16 +5,19 @@ import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
     //부가기능 설정(중복 코드 X, 정의 할 부가 기능만, 타겟 인터페이스의 구현체는 다이나믹 프록시가 생성해줌)
-    Hello target;
+    Object target;
 
-    public UppercaseHandler(Hello target) {
+    public UppercaseHandler(Object target) {
         this.target = target;
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String) method.invoke(target, args);
+    public Object invoke(Object proxy, Method method, java.lang.Object[] args) throws Throwable {
+        Object ret = (String) method.invoke(target, args);
 
-        return ret.toUpperCase();
+        if (ret instanceof String) { //&& method.getNAme().startsWith("say") 메서드 이름이 say로 시작하는 메서드들만 해당하게도 가능
+            return ((String) ret).toUpperCase();
+        }
+        return ret;
     }
 }
